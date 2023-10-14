a63f6850e15d9e15992f63a6d20a87dc17114dab
Heri1504 <144200560+Heri1504@users.noreply.github.com>
2023-10-14 02:35:39 -0600

    Add files via upload

---
+++ if.js
@@ -0,0 +1,33 @@
+if (true){
+    console.log("hola")
+    }
+    hola
+
+     
+
+    if (false) {
+    } else {
+    console.log("adios")
+    }
+    adios 
+
+    
+
+    var num = 3   
+    if (num <5){
+        console.log("menor")
+    } else {
+        console.log("mayor")
+    }
+    menor 
+
+    var num = 1  
+                                        
+
+    if (num >4 ){
+        console.log("mayor")
+    } else {
+        console.log("menor")
+    }
+    menor
+
---
+++ js.js
@@ -0,0 +1,21 @@
+var color = prompt ("ingrese un color") ;
+
+switch(color) { 
+
+    case "green":
+        console.log ("this is a green");
+        break ;
+
+        case "yellow":
+            console.log ("this is a yellow");
+            break ;
+
+            case "blue":
+                console.log ("this is a blue");
+                break ;
+
+                default:
+                    console.log ("this is incorrect color");
+                    break ;
+
+}
